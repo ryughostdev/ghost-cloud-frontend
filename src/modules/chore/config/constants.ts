39cfdc6e5ab8f75c4b4bf59cbaf40efd3c057f20
Leonardo Serrano Alfaro <leovpc@gmail.com>
2024-06-02 03:39:22 -0600

    chore: Update Astro configuration and dependencies for React integration with Tailwind CSS

---
+++ src/modules/chore/config/constants.ts
@@ -0,0 +1,2 @@
+export const appName = "Ghost Cloud";
+export const Server1API = "http://localhost:3000";
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,4 @@
 const { nextui } = require("@nextui-org/react");
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,7 +6,15 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        code: ["JetBrainsMonoNL"],
+      },
+      colors: {
+        primario: "#333333",
+        secundario: "#22d0c9",
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
